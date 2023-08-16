6f8337ba8b6671c20dfb46ad4d83fe7251f3542b
Jutrm <84643295+Jutrm@users.noreply.github.com>
2023-08-16 23:05:55 +0530

    run.py

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-import loggingimport socketimport timedef work():    hostname = socket.gethostname()    ipaddress = socket.gethostbyname(hostname)    logging.basicConfig(format='%(asctime)-2s %(message)s',datefmt='%d/%m/%y %H:%M:%S %p',filename="output1",encoding='utf-8',level=logging.DEBUG)    logging.info("Hostname:"+hostname+" "+"IP:"+ipaddress)if __name__=='__main__':    while True:        work()        time.sleep(60)
+import logging
+import socket
+import time
+def work():
+    hostname = socket.gethostname()
+    ipaddress = socket.gethostbyname(hostname)
+    logging.basicConfig(format='%(asctime)-2s %(message)s',datefmt='%d/%m/%y %H:%M:%S %p',filename="output",encoding='utf-8',level=logging.DEBUG)
+    logging.info("Hostname:"+hostname+" "+"IP:"+ipaddress)
+if __name__=='__main__':
+    while True:
+        work()
+        time.sleep(60)
